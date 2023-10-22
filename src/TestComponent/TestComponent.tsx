f50fec9f42eee3b9bee440d8a07647ac777457da
tanvesh01 <sarvetanvesh01@gmail.com>
2023-10-22 19:47:24 +0530

    feat: add logic to add more images other than the default screenshot

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { TestComponentProps } from "./TestComponent.types";
 
@@ -13,18 +13,40 @@ export const TestComponent: React.FC<TestComponentProps> = ({ isOpen }) => {
     useState<"question" | "feedback" | "bug">("question");
   const [selectedBugType, setSelectedBugType] =
     useState<"low" | "medium" | "high">("low");
+  const hiddenFileInput = useRef(null);
 
   const [isInternalOpen, setIsInternalOpen] = useState<boolean>(false);
   const [defaultScreenshot, setDefaultScreenshot] = useState<string>("");
 
-  const [hideScreenshot, setHideScreenshot] = useState(false);
+  const [hideScreenshot, setHideScreenshot] = useState<boolean[]>([]);
+  const [imageURLS, setImageURLs] = useState([]);
 
-  const handleChange = () => {
-    setHideScreenshot(!hideScreenshot);
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleVisibilityOfScreenshots = (e) => {
+    const index = e.target.value;
+    const newHideScreenshot = [...hideScreenshot];
+    newHideScreenshot[index] = !newHideScreenshot[index];
+    setHideScreenshot(newHideScreenshot);
   };
 
   const onBugTypeChange = (e) => {
     setSelectedBugType(e.target.value);
+  };
+
+  const onFileChange = (e) => {
+    if (e.target.files.length < 1) return;
+    const newURLS = [...e.target.files].map((image: any) =>
+      URL.createObjectURL(image)
+    );
+    const newImageURLS = [...imageURLS, ...newURLS];
+    setHideScreenshot([
+      ...hideScreenshot,
+      ...new Array(newURLS.length).fill(false),
+    ]);
+    setImageURLs(newImageURLS);
   };
 
   useEffect(() => {
@@ -47,12 +69,15 @@ export const TestComponent: React.FC<TestComponentProps> = ({ isOpen }) => {
         const base64image = canvas.toDataURL("image/png");
         // window.location.href = base64image;
         console.log(base64image);
+        setImageURLs([base64image]);
+        setHideScreenshot([true]);
         setDefaultScreenshot(base64image);
         setIsInternalOpen(true);
       });
     }
   }, [isOpen]);
 
+  console.log(imageURLS);
   return (
     <>
       {isInternalOpen &&
@@ -67,25 +92,42 @@ export const TestComponent: React.FC<TestComponentProps> = ({ isOpen }) => {
                 <div className={styles.inputGrid}>
                   <InputContainer />
                   <div className={styles.imageGrid}>
-                    <div className={styles.imageContainer}>
-                      {!hideScreenshot && (
-                        <div className={styles.hideImage}>
-                          <HideEyeIcon />
+                    <div className={styles.scrollableGrid}>
+                      {imageURLS.map((imageSrc, index) => (
+                        <div>
+                          <div className={styles.imageContainer}>
+                            {hideScreenshot[index] && (
+                              <div className={styles.hideImage}>
+                                <HideEyeIcon />
+                              </div>
+                            )}
+                            <img src={imageSrc} height={"auto"} />
+                          </div>
+                          <input
+                            type="checkbox"
+                            onChange={handleVisibilityOfScreenshots}
+                            value={index}
+                            checked={!hideScreenshot[index]}
+                          />
+                          <label>Include image</label>
                         </div>
-                      )}
-                      <img
-                        src={defaultScreenshot}
-                        className=""
-                        width={200}
-                        height={"auto"}
-                      />
+                      ))}
                     </div>
                     <input
-                      type="checkbox"
-                      onChange={handleChange}
-                      checked={hideScreenshot}
+                      type="file"
+                      multiple
+                      accept="image/*"
+                      onChange={onFileChange}
+                      ref={hiddenFileInput}
+                      style={{ display: "none" }}
                     />
-                    <label>Include screenshot</label>
+                    <button
+                      type="button"
+                      className={styles.uploadButton}
+                      onClick={handleClick}
+                    >
+                      Add more Images
+                    </button>
                     <p>Type of bug</p>
                     <div className={`${styles.urgentContainer}`}>
                       <input
